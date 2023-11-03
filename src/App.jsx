0c1633a4008049a631859a7e3a91d9b405c617bd
rajarajagopi <rajagopimca@gmail.com>
2023-11-03 13:23:30 +0530

    Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+const Hello =()=>{
+  return (
+    <div>
+      <p> Hello world</p>
+    </div>
+  )
+}
+
+
 const App = () => {
+  const now = new Date();
+  const a=10;
+  const b=20;
+
 return (
   <div>
-  <p> Hello world</p>
+  <p> Hello world it's a {now.toString()}</p>
+<p> 
+  {a}plus{b} is {a+b}
+</p>
+<h1>Greetings</h1>
+<Hello />
+<Hello />
+<Hello />
+
   </div>
 )
 }
