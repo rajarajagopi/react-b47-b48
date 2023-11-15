3c474295ab8bc3a371eb44df297ca2a4a6290c2d
rajarajagopi <rajagopimca@gmail.com>
2023-11-15 08:31:24 +0530

    conditional rendering using ternary operator

--- src/Component/Note.jsx
+++ src/Component/Note.jsx
@@ -1,13 +1,7 @@
 function Note({note}){
     // const note=props.note;
-    if(note.important)//Conditional rendering
-    {
-        return(
-            <li>{note.content}   ★</li>
-          )
-    }
    return(
-     <li>{note.content}</li>
+     <li>{note.important ? note.content +'★' : note.content}</li>
    )
    }
    export default Note;
