5c9aebac38744502a067be3e8a5dee0f83595a88
rajarajagopi <rajagopimca@gmail.com>
2023-11-17 07:19:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+//function component
+/*import React from 'react';// no need this line from react17 ver..
+
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default App*/
+
+
+
+//-----------------------------------
+//------- This is Class component--------------------------------
+import { Component } from "react";
+class App extends Component {
+render(){
+  return(
+    <div> Hello world...! </div>
+
+  )
+}
+}
+
+export default App;
---
+++ src/example.js
@@ -0,0 +1,20 @@
+//Parent class
+class Component {
+    render (){
+        console.log('render from component');
+    }
+}
+
+let component = new Component();// creating object
+//component.render();// fuction calling
+
+// child class
+class App extends Component{
+    // Method over riding
+    render(){
+        console.log('Method Over ride');
+    }
+}
+
+let app = new App();
+app.render();
