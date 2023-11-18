62f7b5941f6b8979178706d3bd2edceeb640993c
rajarajagopi <rajagopimca@gmail.com>
2023-11-18 13:28:07 +0530

    Create a multiple class component

--- src/App.jsx
+++ src/App.jsx
@@ -41,11 +41,19 @@ function App() {
 }
 */
 //Creating class component
-import React from 'react'
+import { Component } from "react";
+class Hello extends Component{
+  render()
+  {
+    return <div>Hello guest...!</div>
+  }
+}
 class App extends Component{
   render()
   {
-    return <div>Hello Rajagopi</div>
+    return (
+    <div><Hello /></div>
+    )
   }
 }
 export default App
