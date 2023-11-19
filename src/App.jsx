254f55819f1135cce8f657bae5e53fe071f2bbf9
rajarajagopi <rajagopimca@gmail.com>
2023-11-19 06:50:50 +0530

    about the didMount

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,24 @@
-//function component
-/*import React from 'react';// no need this line from react17 ver..
-
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  )
-}
-
-export default App*/
-//-----------------------------------
-//------- This is Class component--------------------------------
-/*import { Component } from "react";
-class App extends Component {
-render(){
-  return(
-    <div> Hello world...! </div>
-
-  )
-}
-}
-
-*/
-
-//--------------Passing props in function component--------------
-/*import React from 'react'
-function Hello({name})
-{
-  return(<div>Hello {name}</div>)
-  
-}
-
-function App() {
-  return (
-    <div>
-      <Hello name="Gopi"/>
-    </div>
-  )
-}
-*/
-//Creating class component
 import { Component } from "react";
-class Hello extends Component{
-  render()
- 
-  {
-    const { name , age }=this.props;
-    return <div>Hello {name} age is {age}...!</div>
-    //return <div>Hello {this.props.name}...!</div>
+class App extends Component {
+  // componentDidmount: Call after the component has been rendered to the Dom
+  componentDidMount(){
+    console.log('component mounted');
   }
-}
-class App extends Component{
-  render()
-  {
-    
-    return (
-    <div><Hello name='Rajgopi' age={34} /></div>
 
-    )
+  // called immediately after the component updated in the DOM
+  // called on state or prop changes
+  componentDidUpdate(){
+    console.log('component state update');
+  }
+
+  componentWillUnmount(){
+    //invoked just before the component is removed from the DOM
+    // releasing memory or resources
+    //detaching the events
+    //cancelling timers
+  }
+  render(){
+    return<div>Hello...!</div>
   }
 }
-export default App
+export default App;
