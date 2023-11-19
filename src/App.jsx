fdcf55963210a7bada0ef6d0cf58656bbd497a12
rajarajagopi <rajagopimca@gmail.com>
2023-11-19 10:06:59 +0530

    exercise - stateful class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-  
-    <div className='mt-4 p-4'>
-      <h1>Hello World!</h1>
-      <p className='redColor'>This paragraph will be in red color.</p>
-    </div>
-  )
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    }
+  }
+
+  handleGoodClick = () => {
+    this.setState({
+      ...this.state,
+      good: this.state.good + 1,
+    })
+  }
+
+  handleNeutralClick = () => {
+    this.setState({
+      ...this.state,
+      neutral: this.state.neutral + 1,
+    })
+  }
+
+  handleBadClick = () => {
+    this.setState({
+      ...this.state,
+      bad: this.state.bad + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={this.handleGoodClick}>good</button>
+        <button onClick={this.handleNeutralClick}>neutral</button>
+        <button onClick={this.handleBadClick}>bad</button>
+
+        <h2>statistics</h2>
+        <p>good { this.state.good }</p>
+        <p>neutral { this.state.neutral }</p>
+        <p>bad { this.state.bad }</p>
+      </div>
+    )
+  }
 }
-export default App;
