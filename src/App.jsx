2a6c84c3acd979e8303eecc4a9da81b283ae9da3
rajarajagopi <rajagopimca@gmail.com>
2023-11-21 05:18:08 +0530

    added zero button to reset the value of the counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   
-  console.log('rendering...', counter);
-  const handleClick = () => {
+  const handlePlusClick = () => {
     // console.log('button clicked');
     setCounter(counter + 1);
   }
 
+  const handleZeroClick = () => {
+    setCounter(0);
+  }
+
+  console.log('rendering...', counter);
+
   return (
- 
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
+      <button onClick={handlePlusClick}>plus</button>
+      <button onClick={handleZeroClick}>zero</button>
     </div>
 
   )
 }
-
 
 export default App;
