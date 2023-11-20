229ae03de795ee8cff1c25459ea467e9d68ac5d9
rajarajagopi <rajagopimca@gmail.com>
2023-11-21 05:05:00 +0530

    adding a simple state counter using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-/*
-  Hooks: 
-    - any function that starts with "use" is called a hook.
-    - they are special functions that are only available while React is rendering
-  
-  To create a counter, where the value is increased as a function of time or at the click of a button.
-*/
-import React from 'react';
 
-function App(props) {
-  // console.log(props);
-  let { counter } = props;
-  console.log(counter);
+import React, { useState } from 'react';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1)
+  }, 1000);
+
+  console.log('rendering...', counter);
+
   return (
     <div>{ counter }</div>
   )
 }
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,51 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-// data
-/*const notes = [
-    {
-        id:1,
-        content:'working with list',
-        important: true
-    },
-    {
-        id:2,
-        content:'Usage of keys',
-        important: false
-    },
-    {
-        id:3,
-        content:'conditional rendering',
-        important: true
-    },
-    {
-        id:4,
-        content:'Handling events',
-        important: true
-    },
-    {
-        id:5,
-        content:'props vs state',
-        important: false
-    }
 
-];
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);*/
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-// create a variable called counter and initialize the value to 1
-let counter = 1;
 
-let refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(<App counter={ counter } />);
-}
-
-//refresh();
-//counter += 1;
-//refresh();
-//counter += 1;
-//refresh();
-
- setInterval(() => {
-     refresh();
-     counter += 1;
- }, 1000);
