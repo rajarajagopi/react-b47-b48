8027fe108d1fb0dfd20a7a64486d10fca196e739
rajarajagopi <rajagopimca@gmail.com>
2023-11-21 07:14:10 +0530

    creating Button component - reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
 import React, { useState } from 'react';
-
 function Display({counter}) {
   return (
     <div>{counter}</div>
   )
 }
+
+function Button({text, handleClick}) {
+  return (
+    <button onClick={handleClick}>{ text }</button>
+  )
+}
+
 function App() {
   const [counter, setCounter] = useState(0);
-
-  
   const handlePlusClick = () => {
     // console.log('button clicked');
     setCounter(counter + 1);
   }
-  const handleZeroClick = () => {
-    setCounter(0);
-  }
-
   const handleMinusClick = () => {
     setCounter(counter - 1);
   }
-
-  console.log('rendering...', counter);
-
+  const handleZeroClick = () => {
+    setCounter(0);
+  }
+  
   return (
     <div>
-      
       <Display counter={counter} />
-      <button onClick={handlePlusClick}>plus</button>
-      <button onClick={handleMinusClick}>minus</button>
-      <button onClick={handleZeroClick}>zero</button>
+      <Button text='plus' handleClick={ handlePlusClick } />
+      <Button text='minus' handleClick={ handleMinusClick } />
+      <Button text='zero' handleClick={ handleZeroClick } />
     </div>
-
   )
 }
-
 export default App;
